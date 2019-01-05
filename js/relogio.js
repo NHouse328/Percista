f471f4b8ad1e95681830126092c90133799e652f
Gabriel Casanova <gabrielcasanova328@gmail.com>
2019-01-04 23:15:59 -0200

    nova janela criatividae
    css e html

--- js/relogio.js
+++ js/relogio.js
@@ -1,9 +1,9 @@
-var canvas = document.getElementById("rc");
+var canvas = document.getElementById("relogio");
 var ctx = canvas.getContext("2d");
 
 var radius = canvas.height / 2;
 ctx.translate(radius, radius);
-radius = radius * 0.90
+radius = radius * 0.90;
 drawClock();
 setInterval(drawClock, 1000);
 
